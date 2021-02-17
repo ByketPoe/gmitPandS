4ecb870043d8e3bb68eb851650bafdf6f9c95007
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-02-17 20:02:16 +0000

    initial commit of random number generator without comments

---
+++ week02/lab2.3.1randomGenerator.py
@@ -0,0 +1,9 @@
+# randomGenerator.py
+# The purpose of this program is to print out a random number between 1 and 10.
+# author: Emma Farrell
+
+import random
+
+number = random.randint(1,10)
+
+print("here is a random number {}".format(number))
