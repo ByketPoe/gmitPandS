7a064bf68ffcdc92632f55f2b4f5a5497ae1d0e0
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-02-17 20:30:55 +0000

    file was not saved, need to push again

--- week02/lab2.3.1randomGenerator.py
+++ week02/lab2.3.1randomGenerator.py
@@ -2,10 +2,14 @@
 # The purpose of this program is to print out a random number between 1 and 10.
 # author: Emma Farrell
 
+# Import the random module. 
+# This will be necessary to use a function to generate a random number.
 import random
 
+# Generate a random number between 1 and 10 using the .randint() function.
+# The two numbers represent the max and min numbers in the range.
+# Assign the result to the variable "number".
 number = random.randint(1,10)
 
-print("here is a random number {}".format(number))
-
-#add comments
+# Print the result.
+print("here is a random number: {}".format(number))
