e0d4ec83cabe33a1ab2623fb427c9c15ebfad82e
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-02-18 19:45:03 +0000

    initial commit of round program

---
+++ week03/lab3.2.1round.py
@@ -0,0 +1,15 @@
+# round.py
+# The purpose of this program is to round a number.
+# It rounds to the nearest number, so caution is advised if accuracy is required.
+# author: Emma Farrell
+
+# The input() function is used to take in a number from the user.
+# This is cast to a float and assigned to a variable "numberToRound".
+numberToRound = float(input("Enter a number of type float: "))
+
+# The round() function is used to round the input number. 
+# The Result is assigned to the variable "roundedNumber".
+roundedNumber = round(numberToRound)
+
+# The rsult is printed using .format() and print(). 
+print('{} rounded is {}'.format(numberToRound, roundedNumber))
