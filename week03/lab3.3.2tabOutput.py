5cda9afa4a469c199bc2227334d2dedcbaeb9ae0
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-02-19 16:28:22 +0000

    initial commit of program with tabs and newlines in strings

---
+++ week03/lab3.3.2tabOutput.py
@@ -0,0 +1,8 @@
+# tabOutput.py
+# The purpose of this program is to deonstrate tab spaces in output strings.
+# author: Emma Farrell
+
+# Within a string, \t creates a space similar to hitting tab on the keyboard.
+# \n creates a new line, like hitting enter on the keyboard. 
+message = 'John said\t"hi"\nI said\t\t"bye"'
+print(message)
