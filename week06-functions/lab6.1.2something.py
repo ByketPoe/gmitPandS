412d81b6716939aa2d5bedd737c6ff6349f9414d
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-03-15 12:43:52 +0000

    lab 6 menu with while loop added

--- week06-functions/lab6.1.2something.py
+++ week06-functions/lab6.1.2something.py
@@ -1,8 +1,27 @@
 def displayMenu():
     print("What would you like to do?\n\t(a) Add a new student\n\t(v) View students\n\t(q) Quit")
-    choice = input("Type one letter (a/v/q): ").strip() # convert to lowercase?
+    choice = input("Type one letter (a/v/q): ").strip().lower() # convert to lowercase?
 
     return choice
 
+def doAdd():
+    # do stuff
+    print("In Adding")
+
+def doView():
+    # do stuff
+    print("In Viewing")
+
+# Main Program
 choice = displayMenu()
+while (choice != "q"):
+    if choice == "a":
+        doAdd()
+    elif choice == "v":
+        doView()
+    elif choice != "q":
+        print("\n\nPlease select either a, v, or q")
+    print("You chose {}".format(choice))
+    choice = displayMenu()
+    
 print("You chose {}".format(choice))
