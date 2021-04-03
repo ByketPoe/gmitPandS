af152c2b53250970a309577eecf3abf9edf7dd45
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-04-04 00:03:20 +0100

    lab 8 files

---
+++ week08-plotting/lab8.1.11counties.py
@@ -0,0 +1,4 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+possibleCounties = ['Mayo', 'Roscommon', 'DirtyDublin', 'Donegal']
---
+++ week08-plotting/lab8.1.5plots.py
@@ -0,0 +1,7 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+xpoints = np.array(range(1, 101))
+ypoints = xpoints ** 2
+plt.plot(xpoints, ypoints)
+plt.show()
