ec4830c1889d440f45799f122462906fb776b8f4
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-04-04 14:39:20 +0100

    week 8 labs 11 to 13

--- week08-plotting/lab8.1.12countiesMod.py
+++ week08-plotting/lab8.1.12countiesMod.py
@@ -5,5 +5,5 @@ possibleCounties = ['Mayo', 'Tyrone', 'Cork', 'Donegal', 'Clare']
 counties = np.random.choice(possibleCounties, p = [0.1, 0.3, 0.2, 0.12, 0.28], size = (100))
 
 unique, counts = np.unique(counties, return_counts = True)
-plt.pie(counts, labels = unique)
+plt.bar(unique, counts)
 plt.show()
